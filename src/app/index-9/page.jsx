225a8ec205d60672d512fe44a58567a6ca3a437f
Muzammil Murad <mohd.m@algoconsulting.net>
2024-08-18 16:43:31 +0530

    deployement fix-6

--- src/app/index-9/page.jsx
+++ src/app/index-9/page.jsx
@@ -11,54 +11,55 @@ import ServiceAreaEight from "@/components/ServiceAreaEight";
 import TeamAreaSix from "@/components/TeamAreaSix";
 import TestimonialEight from "@/components/TestimonialEight";
 
-
 export const metadata = {
   title: "Home || AglieTech - IT Solutions & Technology NEXT JS Template",
   description:
     "Agiletech provide you to build the best agency, app, business, digital, it services, it solutions, network solution, startup, technology, technology company, technology service template.",
 };
 
-
 const page = () => {
   return (
     <>
-      {/* NavbarEight */}
-      <NavbarEight />
-
-      {/* BannerNine */}
-      <BannerNine />
-
-      {/* AboutAreaNine */}
-      <AboutAreaNine />
-
-      {/* ServiceAreaEight */}
-      <ServiceAreaEight />
-
-      {/* RoadmapOne */}
-      <RoadmapOne />
-
-      {/* ContactAreaSeven */}
-      <CounterAreaSix />
-
-      {/* TestimonialEight */}
-      <TestimonialEight />
-
-      {/* GalleryOne */}
-      <GalleryOne />
-
-      {/* ProgressOne */}
-      <ProgressOne />
-
-      {/* TeamAreaSix */}
-      <TeamAreaSix />
-
-      {/* BlogAreaNine */}
-      <BlogAreaNine />
-
-      {/* FooterEight */}
-      <FooterEight />
-
+      <button onClick={() => console.log("index-9")}>test</button>
     </>
+    // <>
+    //   {/* NavbarEight */}
+    //   <NavbarEight />
+
+    //   {/* BannerNine */}
+    //   <BannerNine />
+
+    //   {/* AboutAreaNine */}
+    //   <AboutAreaNine />
+
+    //   {/* ServiceAreaEight */}
+    //   <ServiceAreaEight />
+
+    //   {/* RoadmapOne */}
+    //   <RoadmapOne />
+
+    //   {/* ContactAreaSeven */}
+    //   <CounterAreaSix />
+
+    //   {/* TestimonialEight */}
+    //   <TestimonialEight />
+
+    //   {/* GalleryOne */}
+    //   <GalleryOne />
+
+    //   {/* ProgressOne */}
+    //   <ProgressOne />
+
+    //   {/* TeamAreaSix */}
+    //   <TeamAreaSix />
+
+    //   {/* BlogAreaNine */}
+    //   <BlogAreaNine />
+
+    //   {/* FooterEight */}
+    //   <FooterEight />
+
+    // </>
   );
 };
 
