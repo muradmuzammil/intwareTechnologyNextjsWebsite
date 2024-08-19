fc42eec84d12df93f5f4312c143cf818ff184a08
Muzammil Murad <mohd.m@algoconsulting.net>
2024-08-19 23:18:35 +0530

    deployement 19-08-2024 fix-4

--- src/app/index-9/page.jsx
+++ src/app/index-9/page.jsx
@@ -1,16 +1,16 @@
 //  use client
-import AboutAreaNine from "@/components/AboutAreaNine";
-import BannerNine from "@/components/BannerNine";
-import BlogAreaNine from "@/components/BlogAreaNine";
-import CounterAreaSix from "@/components/CounterAreaSix";
-import FooterEight from "@/components/FooterEight";
-import GalleryOne from "@/components/GalleryOne";
-import NavbarEight from "@/components/NavbarEight";
-import ProgressOne from "@/components/ProgressOne";
-import RoadmapOne from "@/components/RoadmapOne";
-import ServiceAreaEight from "@/components/ServiceAreaEight";
-import TeamAreaSix from "@/components/TeamAreaSix";
-import TestimonialEight from "@/components/TestimonialEight";
+// import AboutAreaNine from "@/components/AboutAreaNine";
+// import BannerNine from "@/components/BannerNine";
+// import BlogAreaNine from "@/components/BlogAreaNine";
+// import CounterAreaSix from "@/components/CounterAreaSix";
+// import FooterEight from "@/components/FooterEight";
+// import GalleryOne from "@/components/GalleryOne";
+// import NavbarEight from "@/components/NavbarEight";
+// import ProgressOne from "@/components/ProgressOne";
+// import RoadmapOne from "@/components/RoadmapOne";
+// import ServiceAreaEight from "@/components/ServiceAreaEight";
+// import TeamAreaSix from "@/components/TeamAreaSix";
+// import TestimonialEight from "@/components/TestimonialEight";
 
 export const metadata = {
   title: "Home || AglieTech - IT Solutions & Technology NEXT JS Template",
